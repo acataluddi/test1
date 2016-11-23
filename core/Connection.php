16ffc83d0c7f195ba892f30a674ffbb4352d20a7
Adriano Cataluddi <acataluddi@metagusto.com>
2016-11-23 10:00:21 +0100

    Added classes. Fixed #1

---
+++ core/Connection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Training\Metagusto\Git;
+
+/**
+ * Class Connection
+ * @package Training\Metagusto\Git
+ * @author Metagûsto <info@metagusto.com>
+ */
+class Connection
+{
+    /**
+     * @var Database
+     */
+    protected $database = null;
+
+    /**
+     * @return Database
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    /**
+     * @param Database $database
+     */
+    public function setDatabase($database)
+    {
+        $this->database = $database;
+    }
+
+}
